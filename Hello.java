cdc19ec7e9ac3d792345932e024cbd716122d762
Nani0406 <51635159+Nani0406@users.noreply.github.com>
2020-06-07 23:00:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 
 	public static void main(String args[]){
 		for (int i=1;i<=10;i++){
-			System.out.println("Hello World welcome12345....."+i);
+			System.out.println("Hello World welcome123....."+i);
 			System.out.println("Thanks for the webhooks demo..");
 			System.out.println("Thanks for the webhooks demo1..");
 			System.out.println("Thanks for the webhooks demonstration..");
