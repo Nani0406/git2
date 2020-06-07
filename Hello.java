d956db7ecf14142c2968e232a62373d3b122c1bd
Nani0406 <51635159+Nani0406@users.noreply.github.com>
2020-06-07 19:51:15 +0530

    removed welcome

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 
 	public static void main(String args[]){
 		for (int i=1;i<=10;i++){
-			System.out.println("Hello World welcome world....."+i);
+			System.out.println("Hello World welcome....."+i);
 			System.out.println("Thanks for the webhooks demo..");
 			System.out.println("Thanks for the webhooks demo1..");
 			System.out.println("Thanks for the webhooks demonstration..");
